3be7de82a2ed37f714c2dc8b31016255c083d942
Jordi Enric <jordi.err@gmail.com>
2026-03-18 15:57:35 +0100

    prettier

--- apps/studio/components/ui/Charts/ComposedChart.utils.tsx
+++ apps/studio/components/ui/Charts/ComposedChart.utils.tsx
@@ -222,7 +222,9 @@ export const CustomTooltip = ({
           ? Number(rawDataPoint[entry.dataKey])
           : entry.value
       const percentage =
-        hasFiniteMaxValue && maxValue > 0 ? ((rawValue / maxValue) * 100).toFixed(valuePrecision) : null
+        hasFiniteMaxValue && maxValue > 0
+          ? ((rawValue / maxValue) * 100).toFixed(valuePrecision)
+          : null
       const isMax = entry.dataKey === maxValueAttribute?.attribute
 
       return (
@@ -374,9 +376,7 @@ export const CustomLabel = ({
     <div className="relative z-10 mx-auto flex flex-col items-center gap-1 text-xs w-full">
       <div className="flex flex-wrap items-center justify-center gap-2">
         <TooltipProvider delayDuration={800}>
-          {items?.map((entry, index) => (
-            <LabelItem key={`${entry.name}-${index}`} entry={entry} />
-          ))}
+          {items?.map((entry, index) => <LabelItem key={`${entry.name}-${index}`} entry={entry} />)}
         </TooltipProvider>
       </div>
     </div>
